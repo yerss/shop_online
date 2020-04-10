0b936482143353921be05d0fc411673241628627
yerss <abdirazakyersin@gmail.com>
2020-04-10 19:46:06 +0600

    Deletion non usable components: catalog, cart

--- src/router/router.js
+++ src/router/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import vCatalog from '../components/catalog/v-catalog'
-import vCart from '../components/cart/v-cart'
 import vSign from '../components/layouts/v-register-login'
 
 Vue.use(Router);
@@ -9,18 +7,7 @@ Vue.use(Router);
 let router = new Router({
     routes:[
         {
-            path: '/',
-            name: 'catalog',
-            component: vCatalog
-        },
-        {
-            path: '/cart',
-            name: 'cart',
-            component: vCart,
-            props: true
-        },
-        {
-            path:'/sign',
+            path:'/',
             name: 'sign',
             component: vSign
         }
