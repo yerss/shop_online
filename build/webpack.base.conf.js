9ee6b207374e86a3502a70357d8e8b1079b29b57
Azamat <Azamatnamialy@gmail.com>
2020-04-15 18:26:41 +0600

    category

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -40,6 +40,24 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.s(c|a)ss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            // Requires sass-loader@^8.0.0
+            options: {
+              implementation: require('sass'),
+              sassOptions: {
+                fiber: require('fibers'),
+                indentedSyntax: true // optional
+              },
+            },
+          },
+        ],
+      },
       ...(config.dev.useEslint ? [createLintingRule()] : []),
       {
         test: /\.vue$/,
--- src/views/layouts/admin-components/categories/list-categories.vue
+++ src/views/layouts/admin-components/categories/list-categories.vue
@@ -1,14 +1,14 @@
 <template>
-  <div>
-    <div class="col-md-12">
-      <div class="col-md-12 grid-margin stretch-card">
+  <section class = "categories">
+    <div class="row">
+      <div class="col-md-12">
         <div class="card">
           <div class="card-body">
-            <h4 class="card-title">Add category</h4>
+            <h4 class="card-title">Добавить категорию</h4>
             <b-form class="forms-sample" id="formItem">
               <div class="row">
                 <div class="col-md-8">
-                  <b-form-group label="Name" label-for="input-name">
+                  <b-form-group label="Наименование" label-for="input-name">
                     <b-form-input type="text"
                                   id="input-name"
                                   placeholder="Name"
@@ -17,7 +17,7 @@
                   </b-form-group>
                 </div>
                 <div class="col-md-4">
-                  <b-form-group label="Short name" label-for="input-alias">
+                  <b-form-group label="Короткое наименование" label-for="input-alias">
                     <b-form-input type="text"
                                   id="input-alias"
                                   placeholder="Short name"
@@ -26,7 +26,7 @@
                   </b-form-group>
                 </div>
               </div>
-              <b-form-group label="Description" label-for="input-description">
+              <b-form-group label="Описание" label-for="input-description">
                 <b-form-textarea type="text"
                                  id="input-description"
                                  placeholder="Description"
@@ -34,7 +34,7 @@
                                  v-model="form.description">
                 </b-form-textarea>
               </b-form-group>
-              <b-form-group label="Keywords" label-for="input-keywords">
+              <b-form-group label="Ключенвые слова" label-for="input-keywords">
                 <b-form-input type="text"
                               id="input-keywords"
                               placeholder="Keywords"
@@ -63,68 +63,119 @@
         </div>
       </div>
     </div>
-    <div class="col-md-12">
-      <b-form-group label="Categories"  label-for="input10">
-        <div class="col-md-12 mt-2 mb-2 border category">
-          <div v-b-toggle="'categories'" class="pl-2 category">
-            <span>All</span>
-          </div>
-          <b-collapse id="categories">
-            <ul class="nav flex-column sub-menu">
-              <li v-for="(category, index) in CATEGORIES" :key="category.id" class="nav-item">
-                <div>
-                  <div v-b-toggle="`categories${category.id}`">
-                    <div class="row">
-                      <div class="col-md-1">
-                      </div>
-                      <div class="col-md-3">
-                        <span>{{category.name}}</span>
-                      </div>
-                      <div class="col-md-2">
-                        <router-link  :to="`/admin/categories/detail/${category.id}`">
-                          <font-awesome-icon icon="eye" class = "blue pointer"/>
-                        </router-link>
-                        /
-                        <a @click = "deleteCategory(category.id, CATEGORIES, index)">
-                          <font-awesome-icon icon="trash-alt" class = "red pointer"/>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                  <b-collapse :id="`categories${category.id}`">
-                    <ul class="nav flex-column sub-menu">
-                      <div>
-                        <li v-for="(subcategory, index) in category.categories" :key="subcategory.id" class="nav-item">
-                          <div class="row">
-                            <div class="col-md-2"></div>
-                            <div class="col-md-2">
-                              <span>{{subcategory.name}}</span>
-                            </div>
-                            <div class="col-md-2">
-                              <router-link  :to="`/admin/categories/detail/${subcategory.id}`">
-                                <font-awesome-icon icon="eye" class = "blue pointer"/>
-                              </router-link>
-                              /
-                              <a @click = "deleteCategory(subcategory.id, category.categories, index)">
-                                <font-awesome-icon icon="trash-alt" class = "red pointer"/>
-                              </a>
-                            </div>
-                          </div>
-                        </li>
-                      </div>
-                    </ul>
-                  </b-collapse>
-                </div>
-              </li>
-            </ul>
-          </b-collapse>
-        </div>
-      </b-form-group>
+    <div class="row">
+     <div class="col-md-12">
+       <v-expansion-panels hover tile>
+         <v-expansion-panel
+           :key="1"
+         >
+           <v-expansion-panel-header>Список категории</v-expansion-panel-header>
+           <v-expansion-panel-content>
+             <v-expansion-panels multiple hover tile flat>
+               <v-expansion-panel
+                 v-for="(category,i) in CATEGORIES"
+                 :key="i"
+               >
+                 <v-expansion-panel-header>{{category.name}}
+                   <div class = "buttons-group d-flex justify-content-end mr-4">
+                     <router-link  class = "mr-2" :to="`/admin/categories/detail/${category.id}`">
+                       <font-awesome-icon icon="eye" class = "blue pointer"/>
+                     </router-link>
+                     <a @click = "deleteCategory(category.id, CATEGORIES, index)">
+                       <font-awesome-icon icon="trash-alt" class = "red pointer"/>
+                     </a>
+                   </div>
+                 </v-expansion-panel-header>
+                 <v-expansion-panel-content>
+                   <v-expansion-panels flat tile multiple hover readonly>
+                     <v-expansion-panel
+                       v-for="(subcategory, index) in category.categories"
+                       :key="subcategory.id"
+                     >
+                       <v-expansion-panel-header expand-icon="">{{subcategory.name}}
+                        <div class="buttons d-flex justify-content-end mr-4">
+                          <router-link  class = "mr-2" :to="`/admin/categories/detail/${subcategory.id}`">
+                            <font-awesome-icon icon="eye" class = "blue pointer"/>
+                          </router-link>
+                          <a @click = "deleteCategory(subcategory.id, category.categories, index)">
+                            <font-awesome-icon icon="trash-alt" class = "red pointer"/>
+                          </a>
+                        </div>
+                       </v-expansion-panel-header>
+                     </v-expansion-panel>
+                   </v-expansion-panels>
+                 </v-expansion-panel-content>
+               </v-expansion-panel>
+             </v-expansion-panels>
+           </v-expansion-panel-content>
+         </v-expansion-panel>
+       </v-expansion-panels>
+
+     </div>
     </div>
-  </div>
+<!--    <div class="row">-->
+<!--      <div class="col-md-12 mt-2 mb-2 border category">-->
+<!--        <b-form-group label="Categories"  label-for="input10">-->
+<!--            <div v-b-toggle="'categories'" class="pl-2 category">-->
+<!--              <span>All</span>-->
+<!--            </div>-->
+<!--            <b-collapse id="categories">-->
+<!--              <ul class="nav flex-column sub-menu">-->
+<!--                <li v-for="(category, index) in CATEGORIES" :key="category.id" class="nav-item">-->
+<!--                  <div>-->
+<!--                    <div v-b-toggle="`categories${category.id}`">-->
+<!--                      <div class="row">-->
+<!--                        <div class="col-md-1">-->
+<!--                        </div>-->
+<!--                        <div class="col-md-3">-->
+<!--                          <span>{{category.name}}</span>-->
+<!--                        </div>-->
+<!--                        <div class="col-md-2">-->
+<!--                          <router-link  :to="`/admin/categories/detail/${category.id}`">-->
+<!--                            <font-awesome-icon icon="eye" class = "blue pointer"/>-->
+<!--                          </router-link>-->
+<!--                          /-->
+<!--                          <a @click = "deleteCategory(category.id, CATEGORIES, index)">-->
+<!--                            <font-awesome-icon icon="trash-alt" class = "red pointer"/>-->
+<!--                          </a>-->
+<!--                        </div>-->
+<!--                      </div>-->
+<!--                    </div>-->
+<!--                    <b-collapse :id="`categories${category.id}`">-->
+<!--                      <ul class="nav flex-column sub-menu">-->
+<!--                        <div>-->
+<!--                          <li v-for="(subcategory, index) in category.categories" :key="subcategory.id" class="nav-item">-->
+<!--                            <div class="row">-->
+<!--                              <div class="col-md-2"></div>-->
+<!--                              <div class="col-md-2">-->
+<!--                                <span>{{subcategory.name}}</span>-->
+<!--                              </div>-->
+<!--                              <div class="col-md-2">-->
+<!--                                <router-link  :to="`/admin/categories/detail/${subcategory.id}`">-->
+<!--                                  <font-awesome-icon icon="eye" class = "blue pointer"/>-->
+<!--                                </router-link>-->
+<!--&lt;!&ndash;                                /&ndash;&gt;-->
+<!--                                <a @click = "deleteCategory(subcategory.id, category.categories, index)">-->
+<!--                                  <font-awesome-icon icon="trash-alt" class = "red pointer"/>-->
+<!--                                </a>-->
+<!--                              </div>-->
+<!--                            </div>-->
+<!--                          </li>-->
+<!--                        </div>-->
+<!--                      </ul>-->
+<!--                    </b-collapse>-->
+<!--                  </div>-->
+<!--                </li>-->
+<!--              </ul>-->
+<!--            </b-collapse>-->
+<!--        </b-form-group>-->
+<!--      </div>-->
+<!--    </div>-->
+  </section>
 </template>
 
 <script>
+
 import {mapActions, mapGetters} from 'vuex'
 
 export default {
@@ -155,8 +206,8 @@ export default {
     deleteItem (source, index) {
       source.splice(index, 1)
     },
-    deleteCategory (id, source, index) {
-      this.DELETE_CATEGORY(id).then(() => {
+    async deleteCategory (id, source, index) {
+      this.DELETE_CATEGORY(id,index).then(() => {
         this.deleteItem(source, index)
       })
     },
@@ -169,6 +220,7 @@ export default {
       fd.append('parent_id', this.form.parent_id)
       fd.append('image', this.form.image)
       this.ADD_CATEGORY(fd)
+      this.GET_CATEGORIES()
     }
   },
   mounted () {
@@ -178,6 +230,9 @@ export default {
 </script>
 
 <style scoped>
+  .v-expansion-panel-header--active {
+    min-height: 50px;
+  }
 .category{
   margin: 3px 0;
   padding: 3px 0;
